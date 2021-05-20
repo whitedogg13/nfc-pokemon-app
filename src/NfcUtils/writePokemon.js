609cd49b53c548f061b441ae4c4d4b7f20b4723d
Richie <whitedogg13@gmail.com>
2021-05-20 13:53:06 +0800

    refactor: use block rather than page in writePokemon.js

--- src/NfcUtils/writePokemon.js
+++ src/NfcUtils/writePokemon.js
@@ -2,50 +2,59 @@
 import NfcManager from 'react-native-nfc-manager';
 
 async function writePokemon(pokemon) {
-  let pageData;
+  let blockData;
   let respBytes = [];
   let allBytes = [];
 
-  // Page 4 (start from 0)
+  // Block 4 (start from 0)
   // - 0, 1: serial number
-  // - 3: basic type
-  // - 4: special type
-  pageData = [0, 0, 0, 0];
-  pageData[0] = pokemon.no >>> 8;
-  pageData[1] = pokemon.no & 0xff;
-  pageData[2] = pokemon.type[0];
-  pageData[3] = pokemon.type[1] || 0;
-  respBytes = await NfcManager.nfcAHandler.transceive([0xa2, 4, ...pageData]);
-  console.warn('page 4', pageData, respBytes);
-  allBytes = [...allBytes, ...pageData];
+  // - 2: basic type
+  // - 3: special type
+  blockData = [0, 0, 0, 0];
+  blockData[0] = pokemon.no >>> 8;
+  blockData[1] = pokemon.no & 0xff;
+  blockData[2] = pokemon.type[0];
+  blockData[3] = pokemon.type[1] || 0;
+  respBytes = await NfcManager.nfcAHandler.transceive([0xa2, 4, ...blockData]);
+  console.warn('block 4', blockData, respBytes);
+  if (respBytes[0] !== 0xa) {
+    throw new Error('fail to write');
+  }
+  allBytes = [...allBytes, ...blockData];
 
-  // Page 5
+  // Block 5
   // - 0: hp
   // - 1: atk
   // - 2: def
   // - 3: 0
-  pageData = [0, 0, 0, 0];
-  pageData[0] = pokemon.hp;
-  pageData[1] = pokemon.atk;
-  pageData[2] = pokemon.def;
-  respBytes = await NfcManager.nfcAHandler.transceive([0xa2, 5, ...pageData]);
-  console.warn('page 5', pageData, respBytes);
-  allBytes = [...allBytes, ...pageData];
+  blockData = [0, 0, 0, 0];
+  blockData[0] = pokemon.hp;
+  blockData[1] = pokemon.atk;
+  blockData[2] = pokemon.def;
+  respBytes = await NfcManager.nfcAHandler.transceive([0xa2, 5, ...blockData]);
+  console.warn('block 5', blockData, respBytes);
+  if (respBytes[0] !== 0xa) {
+    throw new Error('fail to write');
+  }
+  allBytes = [...allBytes, ...blockData];
 
-  // Page 6
+  // Block 6
   // - 0: satk
   // - 1: sdef
   // - 2: spd
   // - 3: 0
-  pageData = [0, 0, 0, 0];
-  pageData[0] = pokemon.satk;
-  pageData[1] = pokemon.sdef;
-  pageData[2] = pokemon.spd;
-  respBytes = await NfcManager.nfcAHandler.transceive([0xa2, 6, ...pageData]);
-  console.warn('page 6', pageData, respBytes);
-  allBytes = [...allBytes, ...pageData];
+  blockData = [0, 0, 0, 0];
+  blockData[0] = pokemon.satk;
+  blockData[1] = pokemon.sdef;
+  blockData[2] = pokemon.spd;
+  respBytes = await NfcManager.nfcAHandler.transceive([0xa2, 6, ...blockData]);
+  console.warn('block 6', blockData, respBytes);
+  if (respBytes[0] !== 0xa) {
+    throw new Error('fail to write');
+  }
+  allBytes = [...allBytes, ...blockData];
 
-  // Page 7 ~ N: the name of the pokemon
+  // Block 7 ~ 11: the name of the pokemon
   let nameBytes = Array.from(pokemon.name).map((_, i) =>
     pokemon.name.charCodeAt(i),
   );
@@ -54,15 +63,18 @@ async function writePokemon(pokemon) {
     nameBytes.push(0);
   }
 
-  const namePageIdx = 7;
+  const nameBlockIdx = 7;
   for (let i = 0; i < 5; i++) {
-    pageData = nameBytes.slice(4 * i, 4 * i + 4);
+    blockData = nameBytes.slice(4 * i, 4 * i + 4);
     respBytes = await NfcManager.nfcAHandler.transceive([
       0xa2,
-      namePageIdx + i,
-      ...pageData,
+      nameBlockIdx + i,
+      ...blockData,
     ]);
-    console.warn(`page ${namePageIdx + i}`, pageData, respBytes);
+    console.warn(`block ${nameBlockIdx + i}`, blockData, respBytes);
+    if (respBytes[0] !== 0xa) {
+      throw new Error('fail to write');
+    }
   }
   allBytes = [...allBytes, ...nameBytes];
 
